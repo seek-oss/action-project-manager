cccd2c7e499fba5b3a184d1c932d624ef919c608
Brendan McMahon <bernos@gmail.com>
2019-10-22 19:12:56 +1100

    Cleaning up

--- index.js
+++ index.js
@@ -5,25 +5,15 @@ async function run() {
     try {
         const token = core.getInput('token');
         const columnId = core.getInput('columnId');
-        const labels = core.getInput('labels');
-        const payload = JSON.stringify(github.context.payload, undefined, 2);
-        console.log(`Payload: ${payload}`);
 
         if (isPullRequest(github.context.payload) || isIssue(github.context.payload)) {
-            
             const api = new github.GitHub(token);
 
-            const params = {
-                content_id: contentId(github.context.payload),
+            return await api.projects.createCard({
                 column_id: columnId,
+                content_id: contentId(github.context.payload),
                 content_type: contentType(github.context.payload)
-            };
-
-            console.log(`params: ${JSON.stringify(params, undefined, 2)}`);
-
-            const result = await api.projects.createCard(params);
-
-            console.log(`Projects: ${JSON.stringify(result)}`);
+            });
         }
 
     } catch(error) {
@@ -32,36 +22,15 @@ async function run() {
 }
 
 const isPullRequest = payload =>
-      false;
+      payload.pull_request != null;
 
 const isIssue = payload =>
       payload.issue != null;
 
 const contentId = payload =>
-      isIssue(payload) ? payload.issue.id : null;
+      isIssue(payload) ? payload.issue.id : (isPullRequest(payload) ? payload.pull_request.id : '');
 
 const contentType = payload =>
-      isIssue(payload) ? 'Issue' : '';
+      isIssue(payload) ? 'Issue' : (isPullRequest(payload) ? 'PullRequest' : '');
 
 run();
-
-
-// {
-//     "assignee": null,
-//     "assignees": [],
-//     "author_association": "COLLABORATOR",
-//     "body": "",
-//     "closed_at": null,
-//     "comments": 0,
-//     "comments_url": "https://api.github.com/repos/SEEK-Jobs/paved-road/issues/1/comments",
-//     "created_at": "2019-10-22T00:50:08Z",
-//     "events_url": "https://api.github.com/repos/SEEK-Jobs/paved-road/issues/1/events",
-//     "html_url": "https://github.com/SEEK-Jobs/paved-road/issues/1",
-//     "id": 510372708,
-//     "labels": [],
-//     "labels_url": "https://api.github.com/repos/SEEK-Jobs/paved-road/issues/1/labels{/name}",
-//     "locked": false,
-//     "milestone": null,
-//     "node_id": "MDU6SXNzdWU1MTAzNzI3MDg=",
-//     "number": 1,
-//     "repository_url": "https://api.github.com/repos/SEEK-Jobs/paved-road",

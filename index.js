2ce77e10ac1f284c8ea2a154d7b44198d2115325
Brendan McMahon <bernos@gmail.com>
2019-10-22 21:14:51 +1100

    Started on labels

--- index.js
+++ index.js
@@ -5,9 +5,22 @@ async function run() {
     try {
         const token = core.getInput('token');
         const columnId = core.getInput('columnId');
+        const labels = core.getInput('labels');
 
         if (isPullRequest(github.context.payload) || isIssue(github.context.payload)) {
             const api = new github.GitHub(token);
+
+            // Ensure all labels exists
+            for (var label of labels) {
+                const existing = await api.issues.getLabel({
+                    owner: github.context.payload.repository.owner.login,
+                    repo: github.context.payload.repository.name,
+                    name: label
+                });
+
+                console.log(`existing:  ${JSON.stringify(existing)}`);
+            }
+
 
             return await api.projects.createCard({
                 column_id: columnId,
@@ -34,3 +47,23 @@ const contentType = payload =>
       isIssue(payload) ? 'Issue' : (isPullRequest(payload) ? 'PullRequest' : '');
 
 run();
+
+// octokit.issues.addLabels({
+//     owner,
+//     repo,
+//     issue_number,
+//     labels
+// })
+
+// octokit.issues.createLabel({
+//     owner,
+//     repo,
+//     name,
+//     color
+// })
+
+// octokit.issues.getLabel({
+//     owner,
+//     repo,
+//     name
+// })

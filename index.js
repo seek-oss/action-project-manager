79f6cf09cd9c55a351a46c617b1da2e226fdef6d
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2019-12-19 10:46:34 +1100

    More debugging

--- index.js
+++ index.js
@@ -35,8 +35,7 @@ async function run() {
             });
         }
     } catch (error) {
-        tools.log.error('MessageType::::', typeof error)
-        if (error.message == 'Project already has the associated issue') {
+        if (error.message.includes('Project already has the associated issue')) {
             tools.log.info('Project already has associated card')
             return
         }

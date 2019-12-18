61618619133f3e6fcc93ec9668dfc7a202da9722
Ashlin Eldridge <ashlineldridge@users.noreply.github.com>
2019-12-19 09:18:10 +1100

    Testing

--- index.js
+++ index.js
@@ -5,36 +5,37 @@ const contentTypePullRequest = 'PullRequest';
 const contentTypeIssue = 'Issue';
 
 async function run() {
-    try {
-        const token = core.getInput('token');
-        const issueColumnId = core.getInput('issueColumnId');
-        const pullRequestColumnId = core.getInput('pullRequestColumnId');
-        const payload = github.context.payload;
-        const api = new github.GitHub(token);
+    console.log("Payload: " + github.context.payload);
+    // try {
+    //     const token = core.getInput('token');
+    //     const issueColumnId = core.getInput('issueColumnId');
+    //     const pullRequestColumnId = core.getInput('pullRequestColumnId');
+    //     const payload = github.context.payload;
+    //     const api = new github.GitHub(token);
 
-        if (isPullRequest(payload)) {
-            const { data: res } = await api.projects.createCard({
-                column_id: pullRequestColumnId,
-                content_id: payload.pull_request.id,
-                content_type: contentTypePullRequest
-            });
-            console.log("Got: " + res)
-            return
-        }
+    //     if (isPullRequest(payload)) {
+    //         const { data: res } = await api.projects.createCard({
+    //             column_id: pullRequestColumnId,
+    //             content_id: payload.pull_request.id,
+    //             content_type: contentTypePullRequest
+    //         });
+    //         console.log("Got: " + res)
+    //         return
+    //     }
 
-        if (isIssue(payload)) {
-            const res = await api.projects.createCard({
-                column_id: issueColumnId,
-                content_id: payload.issue.id,
-                content_type: contentTypeIssue
-            });
-            console.log("Got: " + res)
-            return
-        }
-    } catch(error) {
-        console.log("Error message: " + error.message + ", error: " + error)
-        core.setFailed(error.message);
-    }
+    //     if (isIssue(payload)) {
+    //         const res = await api.projects.createCard({
+    //             column_id: issueColumnId,
+    //             content_id: payload.issue.id,
+    //             content_type: contentTypeIssue
+    //         });
+    //         console.log("Got: " + res)
+    //         return
+    //     }
+    // } catch(error) {
+    //     console.log("Error message: " + error.message + ", error: " + error)
+    //     core.setFailed(error.message);
+    // }
 }
 
 const isPullRequest = payload =>
